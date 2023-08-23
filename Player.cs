94ab6776cc58d2602afb6fad8e97fc4c86563f0b
David-Langat <davidcheruiyot.langat@connect.qut.edu.au>
2023-08-23 22:24:27 +1000

    Added data fields and appropriate methods to child classes of player

--- Player.cs
+++ Player.cs
@@ -27,17 +27,64 @@ public Player(string playerName, string playerType)
     PlayerName = playerName;
     PlayerType = playerType;
 }
-
-
+// We can edit this tomorrow
+/*public int Move
+{
+    set;get;
+}
+//methods
+public virtual int playMove()
+{
+    return Move;
+}*/
 
 }
  class Computerplayer : Player
 {
-
+    //data fields
+    private string computerPlayerName = "Computer";
+    private int computerplayerMove;
+    //properties
+    public string ComputerPlayerName
+    {
+    get { return computerPlayerName; }
+    }
+    public int ComputerPlayerMove
+    {
+    get{return computerplayerMove;}
+    }
+    //methods
+    public override int playMove()
+    {  
+      return ComputerPlayerMove;
+    }
+     //method that selects valid move for computer to play
+    public int SelectValidMove()
+    {
+        //we can discuss how the computer will find out which move is valid
+       return computerplayerMove;
+    }
 
 }
  class Humanplayer : Player
 {
+    //data fields
+    private string HumanplayerName;
 
+    //properties
+    public string HumanPlayerName
+    {
+    get { return HumanplayerName; }
+    set { HumanplayerName = value; }
+    }
+    public int HumanPlayerMove
+    {
+    get; set;
+    }
+    //methods
+    public override int playMove()
+    {
+    return HumanPlayerMove;
+    }
 
 }
